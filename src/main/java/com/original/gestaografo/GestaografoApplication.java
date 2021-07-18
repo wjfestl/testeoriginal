e2a7d3a72915452d51ca44bbe4fa8bf044f7711e
William J Festl <wjfestl@gmail.com>
2021-07-18 14:26:15 -0300

    Projeto Gestao Grafo criado.

---
+++ src/main/java/com/original/gestaografo/GestaografoApplication.java
@@ -0,0 +1,13 @@
+package com.original.gestaografo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaografoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaografoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/original/gestaografo/GestaografoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.original.gestaografo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaografoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
